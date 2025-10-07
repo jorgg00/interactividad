9d29d75dc7a30572fe624fc7613d4623038f6c32
Jorg <jorge.albarran1505@gmail.com>
2025-10-07 10:39:51 -0600

    const radio

--- js/cursor.js
+++ js/cursor.js
@@ -1,9 +1,10 @@
 const FRICTION= 0.1; //mientras más pegado a 1 más rápido, mientras más pegado a 0 más lento
-const RADIO= 50;
+let radio = 50;
 const STROKE_WEIGHT= 10;
 const FILL_COLOR= "rgba(14, 24, 172, 0.1)";
 const BG_COLOR= "rgba(251, 251, 251, 0.1)";
 const STROKE_COLOR= "rgba(114, 20, 20, 0.1)";
+const MIN_RADIO= 30;
 let pos= null;
 
 function setup() {
@@ -16,13 +17,14 @@ function windowResized() {
 }
 
 function update() {
+    radio = abs(sin(frameCount * 0.01)) * 100 + MIN_RADIO;
     pos.lerp(createVector(mouseX, mouseY), FRICTION);
 }
 
 function draw() {
     update();
     background(BG_COLOR);
-    ellipse(pos.x, pos.y, RADIO, RADIO);
+    ellipse(pos.x, pos.y, radio, radio);
     fill(FILL_COLOR);
     stroke(STROKE_COLOR);
     strokeWeight(STROKE_WEIGHT);
