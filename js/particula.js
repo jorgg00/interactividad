9069a2be8030809d03ca9376e14e2b72544919b8
Jorg <jorge.albarran1505@gmail.com>
2025-10-14 11:43:29 -0600

    color random alpha

--- js/particula.js
+++ js/particula.js
@@ -1,26 +1,41 @@
 export default class Particula{
-    constructor(pos){
-        this.pos = pos;
-        this.vel = createVector(-.5,.5)
+    constructor(params={}) {
+        this.pos = params.position || createVector(windowWidth,windowHeight);
+        this.vel = createVector(0,0)
         this.size = 10;
+        this.red = random(255);
+        this.green = random(255);
+        this.blue= random(255);
         this.color = color(255);
         this.alpha = 255;
-        this.life = 100;
+        this.life = params.life || 100;
+        this.setup();
     }
     //Procesos
+    setup(){
+const x = random(-5,5);
+const y = random(-5,5);
+this.vel = createVector(x,y);
+    }
+
     update(){
         this.pos.add(this.vel);
         this.life--; // Decremento de vida de 1 unidad
         this.alpha = map(this.life,100,0,255,0);
-        this.color = color(255, this.alpha);
-        this.size = map(this.life,100,0,50,0);
+        this.color = color(this.red,this.green,this.blue, this.alpha);
+        this.size = map(this.life,100,0,70,0);
     }
 
 //Salida
  draw() {
-   fill(this.color);
+   //fill(this.color);
+   noFill();
+   stroke(this.color);
    ellipse(this.pos.x,this.pos.y,this.size,this.size);
 
     }
     
+    isDead(){
+        return this.life <=0;
+    }
 }
--- js/particulas.js
+++ js/particulas.js
@@ -1,15 +1,27 @@
 import Particula from './particula.js'
 
-let p = null
-
-
-
+let particulas = [];
 window.setup = function(){
-p= new Particula(createVector(windowWidth/2,windowHeight/2));
     createCanvas(windowWidth, windowHeight)
 }
 window.draw = function (){
-    background(0);
-    p.update();
-    p.draw();
+    background(0,50);
+    for(let particula of particulas){
+    particula.update();
+    particula.draw();
+    }
+    
+}
+
+window.mouseMoved = function(){
+    const p = new Particula({
+position: createVector(mouseX, mouseY),
+life: random(150,200)
+    });
+    particulas.push(p); //agrega particula al array
+    
+}
+
+window.windowResized = function(){
+    resizeCanvas(windowWidth,windowHeight);
 }
