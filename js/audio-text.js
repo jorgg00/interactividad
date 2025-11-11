9d968ba852df48f25bb858560e7382173b291e80
Jorg <jorge.albarran1505@gmail.com>
2025-11-11 14:03:03 -0600

    otro noise lol

--- js/audio-text.js
+++ js/audio-text.js
@@ -71,7 +71,7 @@ window.draw = () => {
    vol = amplitude.getLevel();
     // console.log(vol);
 
-    background(int(vol * 255), 20);
+    background(int(vol * 255), 30);
     drawText();
     
     stroke(255,0,0);
@@ -88,6 +88,19 @@ window.draw = () => {
     }
     endShape();
 
+    stroke(255,0,0);
+    noFill();
+
+    translate(center.x+1, center.y+1);
+    beginShape();
+    for (var i = 0; i < 200; i++) {
+      var ang = map(i, 10, 100, 0, TWO_PI);
+      var rad = 200 * noise(i * 0.01, frameCount * 0.005);
+      var x = rad * cos(ang);
+      var y = rad * sin(ang);
+      curveVertex(x, y);
+    }
+    endShape();
     
   
    
